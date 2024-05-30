df77a3f86db195270b78e33c12dfc71bc9d9da5b
Deepak Sharma <dsharma2828@gmail.com>
2024-05-30 14:34:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function mapLetters(str) {
+	//return
+	let obj = {};
+	for (let i = 0; i < str.length; i++) {
+		let t = str[i];
+		if(obj[t] == undefined){
+			obj[t] = [i];
+		}else{
+			obj[t].push(i);
+		}
+	}
+	return obj;
+}
+const str = prompt("Enter str: ");
+alert(JSON.stringify(mapLetters(str)));
